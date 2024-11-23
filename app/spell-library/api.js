736777a19dab5b434f888aa43794da897f16c967
AmberTJC <147658220+AmberTJC@users.noreply.github.com>
2024-11-23 10:42:38 -0700

    Added code for the spell list based of the api tutorial

---
+++ app/spell-library/api.js
@@ -0,0 +1,28 @@
+import { useEffect, useState } from "react";
+import { getAllSpells } from "./api";
+import Spell from "./spell";
+
+
+export default function App() {
+  const [spells, setSpells] = useState([]);
+
+  useEffect(() => {
+    const savedSpells = localStorage.getItem("spells");
+    if (savedSpells) setSpells(JSON.parse(savedSpells));
+    getAllSpells().then((spells) => {
+      setSpells(spells);
+      localStorage.setItem("spells", JSON.stringify(spells));
+    });
+  }, []);
+
+  return (
+    <div className="App">
+      {spells.length === 0 && <span className="loading">Loading...</span>}
+      <ul className="spell-list">
+        {spells.map((spell) => (
+          <Spell key={spell.index} spell={spell} />
+        ))}
+      </ul>
+    </div>
+  );
+}
